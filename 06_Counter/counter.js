61810ce90ba38adde274495aef4e302b80df8551
Sabir-Pathan <pathansbr7@gmail.com>
2024-08-28 14:39:55 +0530

    comment added

--- 06_Counter/counter.js
+++ 06_Counter/counter.js
@@ -3,7 +3,7 @@ let resetbtn = document.querySelector('#reset');
 let h1 = document.querySelector('h1');
 
 let count =1;
-let stopTimer;
+let stopTimer; // global variable declaration for timer function
 
 
 let start = ()=>{
@@ -13,20 +13,21 @@ let start = ()=>{
         h1.textContent = count;
         count++;
     },1000)
-}
+} // function for start counting and change button value start to stop and set interval
+
 let stop = ()=>{
     btn.value = 'Start';
     btn.innerHTML = 'Start';
 h1.innerHTML = `${count}`;
     
     clearInterval(stopTimer);
-}
+}// creat function for stop counting and change button value and clear interval
 
 let reset = ()=>{
     count = 1;
     stop();
 
-}
+} // create function for reset button and call stop function
 btn.addEventListener('click',()=>{
 
     if(btn.value === 'Start'){
@@ -35,9 +36,9 @@ btn.addEventListener('click',()=>{
     else {
         stop();
     }
-});
+}); //start and stop button code call start and stop functions 
 
 resetbtn.addEventListener('click',()=>{
     reset();
     start();
-});
+}); // reset button code call reset and then start function
