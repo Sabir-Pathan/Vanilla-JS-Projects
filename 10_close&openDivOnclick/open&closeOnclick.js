a923753d84caae945c7694011130d86a61202dd3
Sabir-Pathan <pathansbr7@gmail>
2024-08-30 16:47:59 +0530

    Project Complete

--- 10_close&openDivOnclick/open&closeOnclick.js
+++ 10_close&openDivOnclick/open&closeOnclick.js
@@ -1,2 +1,24 @@
 const openBtn = document.getElementById('open-popun-btn')
+let popup = document.querySelector('.popup')
+const closeBtn = document.querySelector('#close-btn')
+const container = document.querySelector('.container')
 
+openBtn.addEventListener('click',()=>{
+popup.style.visibility = 'visible';
+// console.log(e.target.id);
+
+
+console.log(popup);
+})
+
+closeBtn.addEventListener('click',()=>{
+    popup.style.visibility='hidden';
+})
+
+window.addEventListener('click',(e)=>{
+if(e.target === container){
+    popup.style.visibility = 'hidden';
+}
+console.log(e.target);
+
+});
