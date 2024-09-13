07f35690a060ebf691ce4a02603a3d924aad4d2e
Sabir Pathan <pathansbr7@gmail.com>
2024-09-13 21:58:50 +0530

    project complete

--- 14_TodoList/todolist.js
+++ 14_TodoList/todolist.js
@@ -4,14 +4,44 @@ let addBtn = document.getElementById('add-Btn');
 
 let taskInput = document.getElementById('task-name');
 
-let todoList = document.getElementsByClassName('todolist');
+let taskContainer = document.getElementById('task-Container');
 
 addBtn.addEventListener('click',()=>{
+
     let task = document.createElement('div')
+    task.classList.add('task');
+
+    taskContainer.appendChild(task);  
+
     let li = document.createElement ('li');
-    li.innerText = taskInput.value;
-    task.appendChild(li);
-    todoList.appendChild(task)
+    li.innerText = `${taskInput.value}`; 
+    task.append(li);
+
+    let chkBtn = document.createElement('button')  
+    chkBtn.innerHTML = `<i class="fa-solid fa-check"></i>`
+    chkBtn.classList.add('chkTask');
+    task.appendChild(chkBtn);
+
+    let trashBtn = document.createElement('button');
+    trashBtn.innerHTML = `<i class="fa-solid fa-trash"></i>`
+    trashBtn.classList.add('trash');
+    task.appendChild(trashBtn);
+    
+    taskInput.value = '';
+
+
+    chkBtn.addEventListener('click',()=>{
+        chkBtn.parentElement.style.textDecoration='line-through';
+    })
+
+    trashBtn.addEventListener ('click',(e)=>{
+        
+        console.log(e.target.parentElement.parentElement);
+        let target = e.target;
+        target.parentElement.parentElement.remove();
+
+        
+    })
     
     
 })
