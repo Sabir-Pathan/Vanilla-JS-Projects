66334988a54a8aec9b4fd194e9d8ea000d3d7bb6
Sabir Pathan <pathansbr7@gmail.com>
2024-10-05 09:14:00 +0530

    new project added

---
+++ 22_FAQ2/FAQ2.js
@@ -0,0 +1,6 @@
+let answer = document.querySelector('.quation');
+let wraper = document.querySelector('.FAQ-Wraper')
+
+answer.addEventListener('click',()=>{
+    wraper.classList.add('answerdisplay');
+})
