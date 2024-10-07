2d3372152d3164e91bd8909f08fcf7db7b01c1b9
Sabir Pathan <pathansbr7@gmail.com>
2024-10-07 09:08:47 +0530

    project Completed

--- 22_FAQ2/FAQ2.js
+++ 22_FAQ2/FAQ2.js
@@ -1,6 +1,28 @@
-let answer = document.querySelector('.quation');
-let wraper = document.querySelector('.FAQ-Wraper')
+// traversing DOM OPTION-1
+// let QABtn = document.querySelectorAll('.hide-show-Btn');
+// let answer = document.querySelectorAll('.answer');
+// QABtn.forEach(btn => {
+//     btn.addEventListener('click', ()=>{
+//         let parent = btn.parentElement.parentElement.parentElement;
+//         parent.classList.toggle('active');
+        
+        
+//     })
+// });
 
-answer.addEventListener('click',()=>{
-    wraper.classList.add('answerdisplay');
+// Using Selector in DOM OP-2
+
+let FAQ = document.querySelectorAll('.FAQ');
+
+FAQ.forEach((quation)=>{
+    
+    let btn = quation.querySelector('.hide-show-Btn');
+    btn.addEventListener('click',()=>{
+        FAQ.forEach((item)=>{
+            if(item !== quation){
+                item.classList.remove('active')
+            }
+        })
+        quation.classList.toggle('active')
+    }); 
 })
